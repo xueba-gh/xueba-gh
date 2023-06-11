cd787fd2896f7f1c1aeecc678c4be3c9b147b0aa
Valeriana Stormrider <83978246+Jake186@users.noreply.github.com>
2023-06-11 12:14:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const MUSTACHE_MAIN_DIR = './main.mustache';/**
   * Notice the "name" and "date" property.
 */
 let DATA = {
-  name: 'Blessing Tyron',
+  name: '#',
   date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
